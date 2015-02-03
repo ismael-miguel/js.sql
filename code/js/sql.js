db567127dcaa9a666b7d37f789ad249a0f730bd0
ismael-miguel <espertalhao04@hotmail.com>
2015-02-03 17:55:54 +0000

    Update sql.js

--- code/js/sql.js
+++ code/js/sql.js
@@ -4,15 +4,23 @@
 		
 		var xhr=new XMLHttpRequest();
 		
+		xhr.open('POST',file,true);
+		
 		this.toString=function(){return '[Object SqlLink:'+file+']'};
 		this.valueOf=function(){return this.toString();};
 		
 		this.getXHR=function(s){if(s instanceof Sql && s.getLink()==this)return xhr;};
+		
+		this.close=function(){xhr=null;};
 	};
 
 	window.Sql=function(link){
 		
 		var xhr=link.getXHR(this);
+		
+		this.select=function(){
+			
+		};
 		
 	};
 
