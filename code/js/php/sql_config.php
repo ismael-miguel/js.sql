00c7ac4fb17e89313dbebd57f1d8b68e3b4cd20c
ismael-miguel <espertalhao04@hotmail.com>
2015-02-06 14:58:21 +0000

    Create sql_config.php

---
+++ code/js/php/sql_config.php
@@ -0,0 +1,17 @@
+<?
+	return array(
+		'type'=>'mysql',//accepts: mysql, mysqli, sqlite, pdo
+		'host'=>'localhost',
+		'username'=>'root',
+		'password'=>'',
+		'post'=>3306,
+		'db'=>'test'
+	);
+	
+	/*
+	 *Other options:
+	 *
+	 *pdo:
+	 *		-
+	 *
+	 */
