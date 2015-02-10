592ac55546c3d3d36fba5aee044e0ba54e6bb95a
ismael-miguel <espertalhao04@hotmail.com>
2015-02-10 18:01:53 +0000

    Update sql_mysql.php

--- code/js/php/sql_mysql.php
+++ code/js/php/sql_mysql.php
@@ -1,6 +1,45 @@
 <?php
 	
 	if(!mysql_connect($config['host'], $config['username'], $config['password']))
-		return array('error'=>true,'type'=>'link','desc'=>'connection-failed');
+		return array(
+			'error'=>true,
+			'type'=>'link',
+			'desc'=>'connection-failed',
+			'__debug'=>mysql_errno().':'.mysql_error()
+		);
 	
-	if()
+	if(!mysql_select_db())
+		return array(
+			'error'=>true,
+			'type'=>'link',
+			'desc'=>'connection-failed',
+			'__debug'=>mysql_errno().':'.mysql_error()
+		);
+	
+	$results = array();
+	
+	foreach($queries as $k=>$query)
+	{
+		if($result = mysql_query($query))
+		{
+			if($result === true)
+			{
+				$results[$k] = array(
+					'error'=>false,
+					'result'=>true,
+					'insert_id'=>mysql_insert_id(),
+					'affected'=>mysql_affected_rows()
+				);
+			}
+			else
+			{
+				$results[$k] = array(
+					'error'=>false,
+					'num_rows'=>mysql_num_rows($result),
+					'result'=>mysql_fetch_array($config['fetch_style'] ? $config['fetch_style'] : MYSQL_BOTH)
+				);
+			}
+		}
+	}
+	
+	return $results;
