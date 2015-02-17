7c4a8c9196545777ffdef3a5d6795ff29cbff482
ismael-miguel <espertalhao04@hotmail.com>
2015-02-17 11:49:52 +0000

    Update sql.php

--- code/js/php/sql.php
+++ code/js/php/sql.php
@@ -1,12 +1,12 @@
 <?
-	if($_POST['query'])
+	if($_POST['trans'])
 	{
 		$config = include 'sql_config.php';
 		
 		$queries = (array)include 'sql_factory.php';
 		
 		if( isset($queries['error']) )
-			echo  '{"error":true,"error_type":"POST","desc":"wrong-data"}';
+			echo  '{"error":true,"error_type":"POST","desc":"wrong-data","__debug":',json_encode($queries['error']),'}';
 		else
 			echo json_encode( include 'sql_'.$config['type'].'.php' );
 	}
