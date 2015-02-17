2954390f809010e34e74a2474482b6019b29f2aa
ismael-miguel <espertalhao04@hotmail.com>
2015-02-17 11:42:40 +0000

    Update sql_config.php

--- code/js/php/sql_config.php
+++ code/js/php/sql_config.php
@@ -1,17 +1,12 @@
 <?
 	return array(
 		'type'=>'mysql',//accepts: mysql, mysqli, sqlite, pdo
-		'host'=>'localhost',
-		'username'=>'root',
-		'password'=>'',
-		'post'=>3306,
-		'db'=>'test'
+		'mysql'=>array(
+			'host'=>'localhost',
+			'username'=>'root',
+			'password'=>'',
+			'post'=>3306,
+			'db'=>'test'
+		),
+		'allowed'=>'*'
 	);
-	
-	/*
-	 *Other options:
-	 *
-	 *pdo:
-	 *		-
-	 *
-	 */
